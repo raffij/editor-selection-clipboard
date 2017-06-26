99734bafd8e807b78297b6ef1261dc06ac7e9333
Callum <callum@madebymany.co.uk>
2017-06-26 17:22:53 +0100

    simplify focus

--- index.js
+++ index.js
@@ -2,20 +2,8 @@ class Editor {
   constructor () {
     this.register(require('./lib/modules/block_list'))
     this.register(require('./lib/modules/block_types'))
-    this.register(require('./lib/modules/dom_events'))
-    this.register(require('./lib/modules/focus'))
     this.register(require('./lib/modules/history'))
-    this.register(require('./lib/modules/selection'))
     this.register(require('./lib/modules/ui'))
-  }
-
-  get selected () {
-    return this.blockList.filter(block => this.selection.has(block))
-  }
-
-  deleteSelected () {
-    this.history.capture()
-    this.selected.forEach(block => this.blockList.delete(block))
   }
 
   register (module) {
